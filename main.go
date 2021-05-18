d10340e7e47c6b4a052effcb28e52ab360a6cb41
tonouchi510 <tonouchi27@gmail.com>
2021-05-19 00:01:17 +0900

    Update main

--- main.go
+++ main.go
@@ -1,32 +1,59 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
+	userAppService "github.com/tonouchi510/golang-ddd-layout/internal/application/users"
 	"github.com/tonouchi510/golang-ddd-layout/internal/domain/models/users"
-	"github.com/volatiletech/sqlboiler/boil"
+	userRepo "github.com/tonouchi510/golang-ddd-layout/internal/infrastructure/sqlboiler/users"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 func main() {
-	// Open handle to database like normal
-	dsn := "test_user:password@tcp(localhost:3306)/golang_ddd_layout"
+	ctx := context.Background()
+
+	dsn := "test_user:password@tcp(localhost:3306)/golang_ddd_layout?parseTime=true"
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 	boil.SetDB(db)
 
-	userName, err := users.NewUserName("hoge")
+	ur, err := userRepo.NewUserRepository(ctx)
 	if err != nil {
-		fmt.Print(err.Error())
+		fmt.Println(err.Error())
 	}
-	fmt.Println(userName)
-	user, err := users.NewUserByName(userName)
+	us, err := users.NewUserSirvice(ur)
 	if err != nil {
-		fmt.Print(err.Error())
+		fmt.Println(err.Error())
 	}
-	fmt.Println(user)
+
+	uas, err := userAppService.NewUserApplicationService(ur, us)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	c1, err := userAppService.NewRegistorUserCommand("fuga")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	userData, err := uas.Registor(c1)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	c2, err := userAppService.NewGetUserCommand(userData.Id)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	u, err := uas.Get(c2)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(u)
+
 }
