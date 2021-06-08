df9f7e8120b493f66fbbadc5a52d7513ae44951e
tonouchi510 <tonouchi27@gmail.com>
2021-06-09 01:18:01 +0900

    Add gitkeep

--- internal/domain/models/users/user_id.go
+++ internal/domain/models/users/user_id.go
@@ -8,6 +8,8 @@ func NewUserId(value string) (UserId, error) {
 	// ここにルールを指定することで、不正なデータを作成できないようにする
 	if value == "" {
 		return "", fmt.Errorf("ValueError: UserIdが空です。")
+	} else if len(value) != 36 {
+		return "", fmt.Errorf("ValueError: UserIdが不正です。")
 	}
 	id := UserId(value)
 	return id, nil
